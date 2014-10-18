92b6d1008567289931fec201fc94c0a547f872c3
Vinicius Mayer <viniciusmayer@gmail.com>
2014-10-18 23:59:43 +0100

    insert data from generated files, select data to insert into analysis table.

--- scripts/datainserter.py
+++ scripts/datainserter.py
@@ -1,3 +1,4 @@
+from datetime import date
 import psycopg2
 
 
@@ -5,16 +6,22 @@ url = "dbname='pims' user='eleonorvinicius' host='localhost' password='viniciusm
 selectTables = """select table_name from information_schema.tables where table_schema='public' and table_name like 'backend_%'"""
 truncate = """truncate {0} cascade"""
 files = ('insert_into_tipo.sql', 'insert_into_local.sql', 'insert_into_conta.sql', 'insert_into_ponto.sql')
+insert = 'INSERT INTO backend_analise(observacoes, ativo, excluido, data_hora_criacao, usuario_criacao_id,'\
+            'data_hora_atualizacao, usuario_atualizacao_id, periodo, "periodoAnterior_id")'\
+            ' VALUES (''\'imported''\',true,false,now(),(select id from auth_user where email = ''\'viniciusmayer@gmail.com''\'),now(),(select id from auth_user where email = ''\'viniciusmayer@gmail.com''\'),'\
+            '{0}, null)'
+selectPeriodo = """select distinct periodo from backend_ponto"""
 
-print "start"
+print("start")
 try:
     connection = psycopg2.connect(url)
     cursor = connection.cursor()
     cursor.execute(selectTables)
     for row in cursor.fetchall():
         cursor.execute(truncate.format(row[0]))
-except Exception, e:
-    print e
+except Exception as e:
+    print(e)
+    return
 
 for _file in files:
     try:
@@ -23,9 +30,18 @@ for _file in files:
         scriptFile.close()
         cursor.execute(script)
         connection.commit()
-    except Exception, e:
-        print e
+    except Exception as e:
+        print(e)
+        return
+
+try:
+    cursor.execute(selectPeriodo)
+    for row in cursor.fetchall():
+        split = str(row[0]).split('-')
+        cursor.execute(insert.format("'{0}'".format(date(int(split[0]), int(split[1]), int(split[2])))))
+    connection.commit()
+except Exception as e:
+    print(e)
 
 connection.close()
-
-print "end"
+print("end")
