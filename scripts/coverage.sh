be9a48a3db66d3b26a56e00574b9e04b7e5cbb76
Vinicius Mayer <viniciusmayer@gmail.com>
2016-09-07 11:58:53 -0300

    Updating coverage and dbschema reports generation.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,2 +1,3 @@
-coverage run --source='../' --omit="../backend/migrations/*,../pims/*,../scrips/*,../files/*,../docs/*,../*/__*,../backend/admin.py,../backend/forms.py,../manage.py" ../manage.py test backend
+rm -rf ../coverage/*
+coverage run --source='../' --omit="../backend/migrations/*,../pims/*,../scrips/*,../files/*,../docs/*,../*/__*,../*/tests.py,../*/builders.py,../backend/admin.py,../backend/forms.py,../manage.py" ../manage.py test backend
 coverage html --directory="../coverage/"
---
+++ scripts/schemaspy.sh
@@ -0,0 +1,2 @@
+rm -rf ./schemaspy/*
+java -jar schemaSpy_5.0.0.jar -dp postgresql-9.4.1209.jar -t pgsql -host localhost -db pims -s public -u pims -p viniciusmayer -o ../schemaspy/
