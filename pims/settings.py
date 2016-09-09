d2806c6ed0cdb3c6db022cd35245985369eeb3a5
Vinicius Mayer <viniciusmayer@gmail.com>
2016-09-08 21:25:05 -0300

    Configuration to deploy static files.

--- pims/settings.py
+++ pims/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'backend',
+    'django_jenkins'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/html/pims/static/'
+
+
